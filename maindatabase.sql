9ed022ce0e0e4e32dcc1cbf167af9bbab6a9c374
Mathieu <mathieuxavier38150@gmail.com>
2022-10-04 16:10:17 +0200

    created database

--- maindatabase.sql
+++ maindatabase.sql
@@ -4,3 +4,14 @@ CREATE TABLE restaurants (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(64)
 );
+
+CREATE TABLE restaurants_details(
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    distance INT,
+    food VARCHAR(64),
+    consumption VARCHAR(64),
+    price INT,
+    restaurants_id INT
+);
+
+ALTER TABLE restaurants_details ADD FOREIGN KEY (restaurants_id) REFERENCES restaurants(id);

04d0b30b2e720eb38780c4a2eeedda38ab00c88c
Mathieu <mathieuxavier38150@gmail.com>
2022-10-11 15:32:10 +0200

    finished form

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     let submit = document.getElementById('submit')
     let name = document.getElementById('username')
-
+    let form = document.getElementById('username-2')
+    let send = document.getElementById('submit-2')
 
     submit.addEventListener('click', function () {
         alert('Bonjour '+ username.value+' vous êtes connecté !')
         console.log(username.value)
+    })
+    send.addEventListener('click', function () {
+        alert('Bonjour '+ form.value+' vous êtes connecté !')
+        console.log(form.value)
     })
 })
